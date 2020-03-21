c5d5b60a6cf99c9a0f4ea6ceade5f08729c74963
Emelie <scotte2@student.douglascollege.ca>
2020-03-21 14:35:13 -0700

    Fixed Binary64 problem

--- registrationdemo/src/main/java/com/user/wongi5/controller/ItemController.java
+++ registrationdemo/src/main/java/com/user/wongi5/controller/ItemController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.commons.codec.binary.Base64;
 
 import com.user.wongi5.dao.ItemDao;
 import com.user.wongi5.model.Item;
