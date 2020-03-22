6959282150989c5db1987c06ab3812e72e01253c
johnstonm5 <johnstonm5@student.douglascollege.ca>
2020-03-21 17:54:17 -0700

    disabled db manager

--- registrationdemo/src/main/java/com/user/wongi5/config/RootConfig.java
+++ registrationdemo/src/main/java/com/user/wongi5/config/RootConfig.java
@@ -32,7 +32,7 @@ public class RootConfig {
 		//DatabaseManagerSwing.main(new String[] { "--url", "jdbc:derby:memory:testdb", "--user", "", "--password", "" });
 
 		//h2
-		DatabaseManagerSwing.main(new String[] { "--url", "jdbc:h2:mem:testdb", "--user", "sa", "--password", "" });
+		//DatabaseManagerSwing.main(new String[] { "--url", "jdbc:h2:mem:testdb", "--user", "sa", "--password", "" });
 
 	}
 	
